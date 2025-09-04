e5a6815432759548038f36dcc84f1ab574fd9b47
Weston Dransfield <wdrans@gmail.com>
2025-09-03 20:57:58 -0600

    Move relevant_files dir into project-local config dir

--- spec/aircana/configuration_spec.rb
+++ spec/aircana/configuration_spec.rb
@@ -15,8 +15,8 @@ RSpec.describe Aircana::Configuration do
   end
 
   describe "#relevant_project_files_dir" do
-    it "returns the relevant_files directory within project_dir" do
-      expected_path = File.join(config.project_dir, "relevant_files")
+    it "returns the .aircana/relevant_files directory within project_dir" do
+      expected_path = File.join(config.project_dir, ".aircana", "relevant_files")
       expect(config.relevant_project_files_dir).to eq(expected_path)
     end
   end
